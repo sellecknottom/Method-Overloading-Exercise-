9daf0bd70aa068f9f64eb98f5b7cbeb7d7a4dd98
Selleck Elliott <selleck.elliott@gmail.com>
2024-06-07 12:45:41 -0600

    Method Overload

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,5 +6,29 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool c)
+        {
+            /*if (c == true && a + b > 0)
+            {
+               return ($"{a + b} dollars");
+            }
+            else 
+            {
+                return $"{a + b} dollars";
+            }*/
+            if (c)
+            {
+                return ((a + b) > 1) ? a + b + "dollars" : "dollar";
+            }
+            return $"{a + b}";
+        }
     }
 }
